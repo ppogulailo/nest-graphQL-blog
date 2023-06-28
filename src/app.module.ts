292c43151460c851efcdee983186f17da58436cd
PogunGun <pavel.pogulailo@gmail.com>
2023-06-28 22:29:34 +0300

    connect typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '../.env' }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('TYPE_ORM_HOST'),
+        port: config.get('TYPE_ORM_PORT'),
+        username: config.get('TYPE_ORM_USERNAME'),
+        password: config.get('TYPE_ORM_PASS'),
+        database: config.get('TYPE_ORM_DATABASE'),
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
